f3008e0aa7e52860a8fc04b31d6d36f0a6199274
Jed <joshuaelijah.davis@gmail.com>
2023-06-15 12:06:35 -0400

    Edited Procfile

--- maingen.py
+++ maingen.py
@@ -29,7 +29,19 @@ class MyClient(discord.Client):
 
         channel = client.get_channel(1117969208597286993)
 
-        message = channel.get_partial_message(1118916897929437325)
+        embed = discord.Embed()
+        embed.title = "Users With Access"
+        embed.type = "rich"
+        embed.description = "Users who should currently have access to the username update.\nAPI by: <@222073294419918848>\nBot by: <@270232807744208898>"
+        embed.url = "https://discordrollout.nekos.sh/"
+        embed.timestamp = datetime_get.get_timestamp()
+        embed.color = 0x5865f2
+        embed.add_field(name="Nitro: ", value=nitro, inline=False)
+        embed.add_field(name="Non-Nitro: ", value=non_nitro, inline=False)
+
+        embed.set_footer(text="Checks for updates every 60 seconds.")
+        message = await channel.send(embed=embed)
+        print("embed created")
 
         @tasks.loop(seconds=60)  # repeat after every 60 seconds
         async def update():
